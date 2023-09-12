bfdbe1b696379387a7018b63e6b164bd77bc1141
NicacioPereira <vd829020@gmail.com>
2023-09-11 22:27:01 -0300

    PROJECT CREATED

---
+++ src/application/Program.java
@@ -0,0 +1,16 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+        System.out.println("PROJETO DE XADREZ : ");
+    System.out.println("APLICANDO TODOS OS CONCEITOS VISTO ATE AGORA NO CURSO :");
+
+        Locale.setDefault(Locale.US);
+        Scanner scanner = new Scanner(System.in);
+
+        
+    }
+}
